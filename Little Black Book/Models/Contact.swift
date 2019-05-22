fbc822373d6fc393a114c1ec797ec48ab445f529
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-05-22 15:23:08 -0700

    adds crude models and first laid out storyboard

---
+++ Little Black Book/Models/Contact.swift
@@ -0,0 +1,25 @@
+//
+//  Contact.swift
+//  Little Black Book
+//
+//  Created by Jonathan T. Miles on 5/22/19.
+//  Copyright © 2019 Jonathan T. Miles. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+struct Contact {
+    var name: String
+    var company: String?
+    var roleOrField: String?
+    var hobby: String?
+    var picture: UIImage?
+    
+    var emailAddress: String?
+    var phoneNumber: String?
+    var twitterHandle: String? // social media framework object?
+    var physicalAddress: String?
+    
+    // characterization properties go here
+}
---
+++ Little Black Book/Models/Meeting.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting.swift
+//  Little Black Book
+//
+//  Created by Jonathan T. Miles on 5/22/19.
+//  Copyright © 2019 Jonathan T. Miles. All rights reserved.
+//
+
+import Foundation
+
+struct Meeting {
+    var associatedContact: Contact
+    var location: String // maps/location data object?
+    var date: String // Date type?
+    
+    // what fields
+}
